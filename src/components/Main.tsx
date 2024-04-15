275ffbaab20461bec488f1406ef9438a038a4395
Aryan Malik <aryanhunny98@gmail.com>
2024-04-15 20:59:38 +0530

    Simplified checker

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,24 +2,32 @@ export const Main = () => {
     return (
         <>
             <div>
-                <h1 className="text-white font-bold text-4xl">Crypto Address Validator</h1>
-                <div className="mt-5">
-                    <div className="mt-1 mb-5">
-                        <label htmlFor="blockchain" className="text-white text-base font-semibold">Select Blockchain</label>
-                        <select name="blockchain" id="blockchain" className="bg-[#2f2f2f] text-gray-400 text-sm rounded-3xl w-full px-1 py-2 font-semibold shadow-gray-600 shadow-md mt-3">
-                            <option value="bitcoin">Bitcoin</option>
-                            <option value="ethereum">Ethereum</option>
-                            <option value="Polkadot">Polkadot</option>
-                        </select>
-                    </div>
+                <h1 className="text-white font-bold text-4xl mb-8">Crypto Address Validator</h1>
+                <div className="bg-[#2f2f2f] px-8 py-8 rounded-3xl">
                     <div>
-                        <label htmlFor="address" className="text-white text-base font-semibold">Enter Address</label>
-                        <input type="text" name="address" id="address" placeholder="Paste your address here..." className="bg-[#2f2f2f] text-gray-400 text-sm rounded-3xl w-full px-1 py-2 font-semibold shadow-gray-600 shadow-md mt-3"/>
+                        <div className="mb-5">
+                            <label htmlFor="blockchain" className="text-white text-base font-semibold">Select Blockchain</label>
+                            <select name="blockchain" id="blockchain" className="bg-[#2f2f2f] text-gray-200 text-sm rounded-3xl w-full px-2 py-2 shadow-gray-600 shadow-md mt-3">
+                                <option value="bitcoin">Bitcoin</option>
+                                <option value="ethereum">Ethereum</option>
+                                <option value="Polkadot">Polkadot</option>
+                            </select>
+                        </div>
+                        <div>
+                            <label htmlFor="address" className="text-white text-base font-semibold">Enter Address</label>
+                            <input 
+                                type="text" 
+                                name="address" 
+                                id="address" 
+                                placeholder="Paste your address here..." 
+                                className="bg-[#2f2f2f] text-gray-200 text-sm rounded-3xl w-full px-2 py-2 shadow-gray-600 shadow-md mt-3 placeholder-gray-200 focus:placeholder-transparent"
+                                />
+                        </div>
                     </div>
+                    <button className="mt-6 bg-white text-gray-500 rounded-3xl w-full py-2 font-semibold text-sm">
+                        Check Address
+                    </button>
                 </div>
-                <button className="mt-6 bg-white text-gray-500 rounded-3xl w-full py-2 font-semibold text-sm">
-                    Check Address
-                </button>
             </div>
         </>
     );
